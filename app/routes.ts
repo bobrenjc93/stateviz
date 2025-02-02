6cb669ed64936332bfb45ad89db81604eb3bdf73
Bob Ren <bobren@meta.com>
2025-02-01 20:29:34 -0800

    finally

--- app/routes.ts
+++ app/routes.ts
@@ -1,5 +1,6 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-    route("*", "routes/home.tsx"), // meh just do development at /viewer instead of index
+    index("./routes/index.tsx"),
+    route("*", "routes/home.tsx"),
 ] satisfies RouteConfig;
